8a3ddbc69d0a9e21bfb18f4e8a2686ec3b71953a
rikako <jc180215b@gmail.com>
2022-04-14 21:32:50 +0900

    modify_search

--- spec/lib/tasks/update_article_state_spec.rb
+++ spec/lib/tasks/update_article_state_spec.rb
@@ -9,7 +9,7 @@ describe 'article_state:change_to_be_published' do
   end
   # メソッド名やスコープ名の制約があるため自動コードレビューでは実施していません。
   # 参考までに解答例を書いておきます。
-  it 'change_to_be_published' do
+  it 'update_article_state' do
     expect { task.invoke }.to change { Article.published.size }.from(0).to(1)
   end
 end
