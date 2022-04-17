038432b369c5b89d8ab91fcc0781962a35873867
rikako <jc180215b@gmail.com>
2022-04-17 23:36:43 +0900

    modify:pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -50,7 +50,7 @@ class ApplicationController < ActionController::Base
   def render_403
     render file: Rails.root.join('public/403.html'), status: :forbidden, layout: false, content_type: 'text/html'
   end
-  
+
   private
 
   def init_components
