f24818bfb09a13971c84cb6c14a1ce0f49883bbf
rikako <jc180215b@gmail.com>
2022-04-21 20:59:31 +0900

    modify:embed

--- app/models/embed.rb
+++ app/models/embed.rb
@@ -1,19 +1,11 @@
-# == Schema Information
-#
-# Table name: embeds
-#
-#  id         :bigint           not null, primary key
-#  embed_type :integer          default("youtube"), not null
-#  identifier :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-
 class Embed < ApplicationRecord
   has_one :article_block, as: :blockable, dependent: :destroy
   has_one :article, through: :article_block
 
-  enum embed_type: { youtube: 0 }
+  enum embed_type: { youtube: 0, twitter: 1 }
 
   validates :identifier, length: { maximum: 200 }
+  def youtube_identifier
+    identifier.split('/').last if youtube?
+  end
 end
